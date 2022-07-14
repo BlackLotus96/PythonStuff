1b718084631108eedd15866c9bfc377f0a313f90
BlackLotus96 <meddisimone96@gmail.com>
2022-07-15 00:02:49 +0200

    notte

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from sel_functions import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 import time
+import os
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
 
 def main():
-    PATH = "C:/Users/simon/OneDrive/Desktop/Python/PythonDriver/chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://www.eurobet.it/it/scommesse/#!/")
-    wait_class_1 = ClassWait(driver, By.ID, "onetrust-accept-btn-handler", 20)
-    wait_class_1.WaitConditions()
-    wait_class_2 = ClassWait(driver, By.ID, "att_lightbox_close", 20)
-    wait_class_2.WaitConditions()
+    name = 'chromedriver.exe'
+    root = 'C:/Users/simon/OneDrive/Desktop/Python'
+    path = find(name, root).replace('\\', '/')
 
+    driver = webdriver.Chrome(path)
+    driver.get("https://www.skyscanner.it/")
+    cookie = ClassWait(driver, By.ID, "acceptCookieButton", 20)
+    cookie.WaitConditions()
+
+
+    destination = ClassWait(driver, By.ID, "react-autowhatever-fsc-destination-search", 20)
+    destination.WaitConditions()
+    destination.clear()
+    destination.send_keys("Lisbona")
+
+    time.sleep(5000)
 
 
 
