3719a9d05b65be102c974b01e6ddef0a0469b652
BlackLotus96 <meddisimone96@gmail.com>
2022-07-14 16:56:23 +0200

    changed sel_functions

--- sel_functions.py
+++ sel_functions.py
@@ -1,15 +1,5 @@
 import json
 class ClassSelenium:
-    attr = '{
-    'class name':'class name',
-    'css selector' : 'css selector',
-    'id' : 'id',
-    'link text' : 'link text',
-    'name' : 'name',
-    'partial link text' : 'partial link text',
-    'tag name' : 'tag name',
-    'xpath' : 'xpath'
-    }'
     attributes_json = json.load(x)
 
   def __init__(self, driver, type_attr, name_attr, wait_time):
@@ -21,6 +11,7 @@ class ClassSelenium:
   def WaitConditions(self):
     try:
       cookie_accepted = WebDriverWait(self.driver, 10).until(
-          EC.presence_of_element_located((, "onetrust-accept-btn-handler")) 
+          EC.presence_of_element_located((, "onetrust-accept-btn-handler"))
           )
     finally:
+        continue
