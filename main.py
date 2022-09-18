9741838431a13de002d66f971cdff72222ad6188
BlackLotus96 <meddisimone96@gmail.com>
2022-09-19 00:30:27 +0200

    commit da pc casa

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+import sys
+#
 import time
 import os
 
@@ -16,27 +17,46 @@ def find(name, path):
 def main():
     #options = Options()
     #options.headless = True
-
     name = 'chromedriver.exe'
-    root = 'C:/Users/simon/OneDrive/Desktop/Python'
+    root = r"C:\Users\simon\OneDrive\Desktop\Python\PythonDriver"
     path = find(name, root).replace('\\', '/')
+    #chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(path)
-    driver.get("https://www.kayak.it/")
-    time.sleep(5)
-    cookie = ClassWait(driver, By.XPATH, "/html/body/div[5]/div/div[3]/div/div/div[2]/div/div/div[1]/button", 10)
-    cookie.WaitConditionsButtons()
-    cookie = ClassWait(driver, By.XPATH, "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div[2]/div/div/div/div/div[1]/div[2]/div/div[3]/div/div/input", 5)
-    cookie.WaitConditionsInputBox("lisbona")
+    driver.get("https://www.cardmarket.com/it/Magic")
+    #time.sleep(20)
+    #sys.exit()
 
-    time.sleep(20)
+    cookie = ClassWait(driver, By.XPATH, "/html/body/header/div[1]/div/div/form/button", 10)
+    cookie.WaitConditionsButtons()
+
+    tbody_XPATH = "/html/body/main/section[2]/div/div[2]/div[1]/table/tbody"
+    tbody = driver.find_element("xpath", tbody_XPATH)
+
+    for i_tr in range(1, len(tbody.find_elements(By.XPATH , "./*"))+1):
+        tr_XPATH = tbody_XPATH+"/tr["+str(i_tr)+"]"
+        tr = driver.find_element("xpath", tr_XPATH)
+        print("@@@@@@@@@@@@@@@@@@")
+        for i_td in range(1, len(tr.find_elements(By.XPATH , "./*"))+1):
+            td_XPATH = tr_XPATH+"/td["+str(i_td)+"]"
+            td = driver.find_element("xpath", td_XPATH)
+            print(td.text)
+
+    '''
+    element = driver.find_element("xpath", "/html/body/main/section[2]/div/div[2]/div[1]/table/tbody/tr[4]")
+    print(element)
+    text = driver.find_element("xpath", "/html/body/main/section[2]/div/div[2]/div[1]/table/tbody/tr[4]/td[4]")
+    print(text.text)
+    '''
+
+    while True:
+        pass
+    '''
+    cookie = ClassWait(driver, By.XPATH, "/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div/input", 5)
+    cookie.WaitConditionsInputBox("lisbona")
+    '''
 
     #destination = ClassWait(driver, By.CLASS_NAME, "BpkInput_bpk-input__ZDdkM SingleDestControls_fsc-large-above-tablet__Mjg0Y SingleDestControls_fsc-docked-middle-above-tablet__MmIwM SingleDestControls_fsc-docked-last-on-tablet__MDI0N LocationSelector_fsc-location-input__NDRiO", 5)
     #destination.WaitConditionsInputBox()
-
-
-
-
-
 
 
 
