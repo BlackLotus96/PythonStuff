923ceff886725d5bcdc03ea49c0d7dee3147ed3f
BlackLotus96 <meddisimone96@gmail.com>
2022-09-19 18:22:29 +0200

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import sys
-#
+##
 import time
 import os
 
