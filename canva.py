2992ea999f9d7727001adae0933c969e3b221308
BlackLotus96 <meddisimone96@gmail.com>
2022-09-30 16:52:33 +0200

    f

--- canva.py
+++ canva.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import os
 
 
@@ -9,23 +11,50 @@ def find(name, path):
             return os.path.join(root, name)
 
 def screen(driver_name, driver_path):
+    global root
+    list_entry = []
     root= tk.Tk()
+    root.geometry("400x300")
+    root.title("Breccia's sorter")
 
-    canvas = tk.Canvas(root, width = 400, height = 300)
-    canvas.pack()
+    def addLink():
 
-    entry = tk.Entry(root)
-    canvas.create_window(200, 140, window=entry)
+        entry = tk.Entry(root, width=70)
+        entry.pack(pady=25)
+        list_entry.append(entry)
+        w = 400
+        h = 300+len(list_entry)*30
+        root.geometry(f"{w}x{h}")
 
-    path = find(driver_name, driver_path).replace('\\', '/')
-    driver = webdriver.Chrome(path)
 
-    def getLink ():
+    button_add = tk.Button(root, text='Add Link', command=addLink)
+    button_add.pack()
+
+    # Entry box
+    entry = tk.Entry(root, width=70)
+    entry.pack()
+    list_entry.append(entry)
+
+
+
+    # Button
+    def getLink():
         x = entry.get()
         driver.get(x)
+        root.destroy()
 
-    button = tk.Button(text='Go to Link', command=getLink)
-    canvas.create_window(200, 180, window=button)
+    button = tk.Button(root, text='Go to Link', command=getLink)
+    button.pack()
+
+
+    #canvas.create_window(200, 180, window=button)
+
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+
+    path = find(driver_name, driver_path).replace('\\', '/')
+    driver = webdriver.Chrome(path, chrome_options=options)
 
     root.mainloop()
     return driver
--- main.py
+++ main.py
@@ -2,25 +2,26 @@ from sel_functions import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+
 import sys
 import canva
 ##
 import time
-
+#
 def main(driver):
     cookie = ClassWait(driver, By.XPATH, "/html/body/header/div[1]/div/div/form/button", 10)
     cookie.WaitConditionsButtons()
 
     tbody_XPATH = "/html/body/main/section[2]/div/div[2]/div[1]/table/tbody"
-    tbody = driver.find_element("xpath", tbody_XPATH)
+    tbody_XPATH_2 = "/html/body/main/section/div[3]/div[2]"
+    tbody = driver.find_element("xpath", tbody_XPATH_2)
 
     for i_tr in range(1, len(tbody.find_elements(By.XPATH , "./*"))+1):
-        tr_XPATH = tbody_XPATH+"/tr["+str(i_tr)+"]"
+        tr_XPATH = tbody_XPATH_2+"/div["+str(i_tr)+"]"
         tr = driver.find_element(By.XPATH, tr_XPATH)
         print("@@@@@@@@@@@@@@@@@@")
         for i_td in range(1, len(tr.find_elements(By.XPATH , "./*"))+1):
-            td_XPATH = tr_XPATH+"/td["+str(i_td)+"]"
+            td_XPATH = tr_XPATH+"/div["+str(i_td)+"]"
             td = driver.find_element(By.XPATH, td_XPATH)
             print(td.text)
 
@@ -42,5 +43,8 @@ def main(driver):
 if __name__ == "__main__":
     driver_name = 'chromedriver.exe'
     driver_path = r"C:\Users\simon\OneDrive\Desktop\Python\PythonDriver"
+
     driver = canva.screen(driver_name, driver_path)
+    print("prova1")
     main(driver)
+    print("prova2")
