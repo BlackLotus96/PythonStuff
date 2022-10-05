94bd49dd1577086dd9126803f6853b7075876941
BlackLotus96 <simonemeddi96@gmail.com>
2022-10-05 09:27:34 +0200

    Delete gotoenv.bat

--- gotoenv.bat
+++
@@ -1,5 +0,0 @@
-set root=C:\Users\simone.meddi\Desktop\Simone\Python\PythonDrivers\env\Scripts
-set root2=C:\Users\simone.meddi\Documents\GitHub\PythonStuff
-CD %root%
-call activate.bat & CD %root2% & python main.py
-cmd /k
