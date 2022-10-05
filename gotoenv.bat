eb5bd6c120d19ca49486587e208fc7384032c532
BlackLotus96 <simonemeddi96@gmail.com>
2022-10-05 08:57:52 +0200

    Update gotoenv.bat

--- gotoenv.bat
+++ gotoenv.bat
@@ -1,5 +1,5 @@
-set root=C:\Users\simon\OneDrive\Desktop\Python\PythonDriver\env\Scripts
-set root2=C:\Users\simon\OneDrive\Desktop\Python\PythonStuff
+set root=C:\Users\simone.meddi\Desktop\Simone\Python\PythonDrivers\env\Scripts
+set root2=C:\Users\simone.meddi\Documents\GitHub\PythonStuff
 CD %root%
 call activate.bat & CD %root2% & python main.py
 cmd /k
